d0c5936d902c198a16d424e65cfc22a14cf6bd51
realsanya <sashamorozova01@mail.ru>
2023-05-19 17:32:04 +0300

    added validator criterias

--- flask-server/ml/text_validator.py
+++ flask-server/ml/text_validator.py
@@ -19,7 +19,7 @@ def text_validator(text):
     "value": len(sentences_with_title_matching_word) != 0
   })
 
-  # актуальность
+  # постановка и обоснование актуальности проблемы
   relevance_words = []
   with open('data/relevance.txt', 'r') as file:
     relevance_words = file.read().splitlines()
@@ -31,8 +31,56 @@ def text_validator(text):
           sentences_with_relevance_word.append(sen)
   
   result.append({
-    "name": 'Актуальность',
+    "name": 'Постановка и обоснование актуальности проблемы',
     "value": len(sentences_with_relevance_word) != 0
+  })
+
+  # оценка степени изученности проблемы
+  novelty_words = []
+  with open('data/novelty.txt', 'r') as file:
+    novelty_words = file.read().splitlines()
+
+  sentences_with_novelty_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(novelty_words))>0:
+          sentences_with_novelty_word.append(sen)
+  
+  result.append({
+    "name": 'Оценка степени изученности проблемы',
+    "value": len(sentences_with_novelty_word) != 0
+  })
+
+  # характеристика методов
+  methods_words = []
+  with open('data/methods.txt', 'r') as file:
+    methods_words = file.read().splitlines()
+
+  sentences_with_methods_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(methods_words))>0:
+          sentences_with_methods_word.append(sen)
+  
+  result.append({
+    "name": 'Характеристика методов',
+    "value": len(sentences_with_methods_word) != 0
+  })
+
+  # структура, логика и стиль изложения
+  exposition_words = []
+  with open('data/exposition.txt', 'r') as file:
+    exposition_words = file.read().splitlines()
+
+  sentences_with_exposition_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(exposition_words))>0:
+          sentences_with_exposition_word.append(sen)
+  
+  result.append({
+    "name": 'Структура, логика и стиль изложения',
+    "value": len(sentences_with_exposition_word) != 0
   })
 
   # аргументированность выводов
@@ -49,6 +97,70 @@ def text_validator(text):
   result.append({
     "name": 'Аргументированность выводов',
     "value": len(sentences_with_argumentation_word) != 0
+  })
+
+  # состояние научно-справочного аппарата
+  apparatus_words = []
+  with open('data/apparatus.txt', 'r') as file:
+    apparatus_words = file.read().splitlines()
+
+  sentences_with_apparatus_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(apparatus_words))>0:
+          sentences_with_apparatus_word.append(sen)
+  
+  result.append({
+    "name": 'Cостояние научно-справочного аппарата',
+    "value": len(sentences_with_apparatus_word) != 0
+  })
+
+  # качество и обоснованность иллюстративного материала
+  illustrations_words = []
+  with open('data/illustrations.txt', 'r') as file:
+    illustrations_words = file.read().splitlines()
+
+  sentences_with_illustrations_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(illustrations_words))>0:
+          sentences_with_illustrations_word.append(sen)
+  
+  result.append({
+    "name": 'Качество и обоснованность иллюстративного материала',
+    "value": len(sentences_with_illustrations_word) != 0
+  })
+
+  # корректность цитирования
+  citation_words = []
+  with open('data/citation.txt', 'r') as file:
+    citation_words = file.read().splitlines()
+
+  sentences_with_citation_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(citation_words))>0:
+          sentences_with_citation_word.append(sen)
+  
+  result.append({
+    "name": 'Корректность цитирования',
+    "value": len(sentences_with_citation_word) != 0
+  })
+
+ # общие замечания и рекомендации
+  remarks_words = []
+  with open('data/remarks.txt', 'r') as file:
+    remarks_words = file.read().splitlines()
+
+  sentences_with_remarks_word = []
+  for sen in sent_tokenize(text):
+      l = word_tokenize(sen)
+      if len(set(l).intersection(remarks_words))>0:
+          sentences_with_remarks_word.append(sen)
+  
+  result.append({
+    "name": 'Общие замечания и рекомендации',
+    "value": len(sentences_with_remarks_word) != 0
   })
 
   return result
